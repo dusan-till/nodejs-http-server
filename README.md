80031cb2aeb2969fc6b29726111902ee35ddcb24
Dušan Till <tilldusan30@gmail.com>
2021-11-29 20:25:48 +0100

    Update dev1.0.2

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 An HTTP Server running on NodeJS [dev]
 
 ## Updates
+### dev1.0.2
+* Better file reading
+* Automaticaly substitutes paths leading to a directory with that directory's index.html file
+* Now treats the html folder as root for the website
+* Returns 404 error if the file is not found
 ### dev1.0.1
 * Adds ability to read files and recognize content types
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs/promises');
 const path = require('path');
 const url = require('url');
-const port = 80;
+const port = 8080;
 const content_types = {
     ".css": "text/css",
     ".html": "text/html",
@@ -23,11 +23,40 @@ const content_types = {
 }
 
 const requestListener = async function (req, res) {
-    let path_ = __dirname + url.parse(req.url, `http://${req.headers.host}`).pathname;
-    console.log(path_)
-    let file = await fs.readFile(path_);
+    //parsing request
+    let path_ = __dirname + "/http" + url.parse(req.url, `http://${req.headers.host}`).pathname;
+
+    //finding the file
+    let notfound = false;
+    let file;
+    try {
+        let stat = await fs.stat(path_);
+        if(stat.isDirectory()) {
+            if(path_[path_.length-1] == path.delimiter) {
+                path_ += "index.html";
+            } else {
+                path_ += "/index.html";
+            }
+        } else if(stat.isFile()) {
+            file = await fs.readFile(path_);
+        } else {
+            file = "";
+            notfound = true;
+        }
+    }
+    catch(err) {
+        console.log(err.code);
+        file = "";
+        notfound = true;
+    }
+
+    //sending response
     res.setHeader("Content-Type", content_types[path.extname(path_)] || "text/plain");
-    res.writeHead(200);
+    if(notfound) {
+        res.writeHead(404);
+    } else {
+        res.writeHead(200);
+    }
     res.end(file);
 }
 
